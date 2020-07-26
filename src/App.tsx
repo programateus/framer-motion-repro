909da47c227196a84f8f7775dfec52549b2d31f1
Mateus Barbosa <mateusbds222@gmail.com>
2020-07-25 22:40:37 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       {/* I want to pass placeholder as props here */}
-      {/* <Input placeholder="Nome" /> */}
+      {/* <Input placeholder="name" /> */}
       <Input />
     </div>
   );
